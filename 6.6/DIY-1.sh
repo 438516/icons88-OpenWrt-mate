d90a6e24be232312ee80871fd2c85d3774dd56b5
438516 <70022856+438516@users.noreply.github.com>
2024-09-08 10:47:57 +0800

    Update DIY-1.sh

--- 6.6/DIY-1.sh
+++ 6.6/DIY-1.sh
@@ -23,3 +23,6 @@ sed -i '$a\src-git openclash https://github.com/vernesong/OpenClash' ./feeds.con
 # 添加PassWall插件
 echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
 echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
+
+#添加mihomo插件
+echo "src-git mihomo https://github.com/morytyann/OpenWrt-mihomo.git;main" >> "feeds.conf.default"
